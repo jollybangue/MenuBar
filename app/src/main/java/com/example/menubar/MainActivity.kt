b9a1a29f2d9c43a72452c62b39674ae37079c215
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-09-25 17:40:28 -0600

    Initial commit
    A small Android app to experiment the Menu Bar features.

---
+++ app/src/main/java/com/example/menubar/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.example.menubar
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+        override fun onCreateOptionsMenu(menu: Menu): Boolean {
+            val inflater: MenuInflater = menuInflater
+            inflater.inflate(R.menu.appbar_menu, menu)
+            return true
+        }
+
+
+        override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+
+            R.id.mContact -> {
+                Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.mFavorite -> {
+                Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.mSettings -> {
+                Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            R.id.mFeedback -> {
+                Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
+                true
+            }
+
+            else -> {
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                super.onOptionsItemSelected(item)
+            }
+        }
+
+}
