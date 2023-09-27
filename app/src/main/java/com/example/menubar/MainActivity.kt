d6b9d1cf7d30c58d129e92d171636289ded39552
jollybangue <141067435+jollybangue@users.noreply.github.com>
2023-09-27 11:40:06 -0600

    Update MainActivity.kt

--- app/src/main/java/com/example/menubar/MainActivity.kt
+++ app/src/main/java/com/example/menubar/MainActivity.kt
@@ -13,40 +13,37 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 
-        override fun onCreateOptionsMenu(menu: Menu): Boolean {
-            val inflater: MenuInflater = menuInflater
-            inflater.inflate(R.menu.appbar_menu, menu)
-            return true
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.appbar_menu, menu)
+        return true
+    }
+
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+
+        R.id.mContact -> {
+            Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
+            true
         }
 
-
-        override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
-
-            R.id.mContact -> {
-                Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
-                true
-            }
-
-            R.id.mFavorite -> {
-                Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
-                true
-            }
-
-            R.id.mSettings -> {
-                Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
-                true
-            }
-
-            R.id.mFeedback -> {
-                Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
-                true
-            }
-
-            else -> {
-                // If we got here, the user's action was not recognized.
-                // Invoke the superclass to handle it.
-                super.onOptionsItemSelected(item)
-            }
+        R.id.mFavorite -> {
+            Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
+            true
         }
 
+        R.id.mSettings -> {
+            Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
+            true
+        }
+
+        R.id.mFeedback -> {
+            Toast.makeText(applicationContext, "You clicked on ${item.title}", Toast.LENGTH_SHORT).show()
+            true
+        } else -> {
+            // If we got here, the user's action was not recognized.
+            // Invoke the superclass to handle it.
+            super.onOptionsItemSelected(item)
+            }
+    }
 }
